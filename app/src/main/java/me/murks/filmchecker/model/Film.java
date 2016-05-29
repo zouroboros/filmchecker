d030fc1caf2e805f733a6ea21e60a34806776111
zouroboros <zouroboros@gmail.com>
2016-05-29 18:47:47 +0200

    Added comments and fixed small typos.

--- app/src/main/java/me/murks/filmchecker/model/Film.java
+++ app/src/main/java/me/murks/filmchecker/model/Film.java
@@ -1,21 +1,37 @@
 package me.murks.filmchecker.model;
 
 /**
- * Created by mark on 28.05.16.
+ * Model for a film order
+ * @author zouroboros
+ * @version 0.1 2016-05-29
  */
 public class Film {
+
     private final String orderNumber;
     private final String shopId;
 
+    /**
+     * Constructs a new Film with the given order number and the given shop id
+     * @param orderNumber The order number
+     * @param shopId The shop id
+     */
     public Film(String orderNumber, String shopId) {
         this.orderNumber = orderNumber;
         this.shopId = shopId;
     }
 
+    /**
+     * Returns the order number
+     * @return The order number as {@see String}
+     */
     public String getOrderNumber() {
         return orderNumber;
     }
 
+    /**
+     * Returns the shop id
+     * @return The shop id as {@see String}
+     */
     public String getShopId() {
         return shopId;
     }
