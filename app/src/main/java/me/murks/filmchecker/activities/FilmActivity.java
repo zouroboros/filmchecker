63ea3fcc6c9a19a53945c6e12ff74eb905d58c18
Zouroboros <zouroboros@zoho.eu>
2019-06-30 23:09:01 +0200

    Before opening a stores tracking page the tracking code is copied to the clipboard.

--- app/src/main/java/me/murks/filmchecker/activities/FilmActivity.java
+++ app/src/main/java/me/murks/filmchecker/activities/FilmActivity.java
@@ -9,15 +9,18 @@ import me.murks.filmchecker.model.Film;
 import me.murks.filmchecker.model.FilmStatus;
 import me.murks.filmchecker.model.StoreModel;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.format.DateFormat;
-import android.text.format.DateUtils;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 import java.util.TimeZone;
@@ -76,7 +79,7 @@ public class FilmActivity extends AppCompatActivity {
         }, film);
     }
 
-    private void bindFilm(Film film, FilmStatus status) {
+    private void bindFilm(final Film film, FilmStatus status) {
         final StoreModel model = app.getStoreModelForFilm(film);
 
         shopName.setText(model.getStoreName());
@@ -104,6 +107,18 @@ public class FilmActivity extends AppCompatActivity {
         openStoreTrackingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(model.getTrackingString(film) != null) {
+                    ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    ClipData clip = ClipData.newPlainText(
+                            String.format(getString(R.string.trackingStringPasteLabel),
+                                    getString(model.getStoreName())),
+                            model.getTrackingString(film));
+                    clipboard.setPrimaryClip(clip);
+
+                    Toast.makeText(view.getContext(), R.string.trackingStringPasteToast,
+                            Toast.LENGTH_SHORT).show();
+                }
+
                 Intent intent = new Intent(Intent.ACTION_VIEW,
                         Uri.parse(getResources().getString(model.getStoreUrl())));
                 startActivity(intent);
